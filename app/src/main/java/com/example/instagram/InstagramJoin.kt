1936cd491d262cc01370737ab00d328ffa98957d
soyeon <sykim0659@naver.com>
2024-09-02 01:24:21 +0900

    인스타 그램 - 로그인 화면 구현

---
+++ app/src/main/java/com/example/instagram/InstagramJoin.kt
@@ -0,0 +1,12 @@
+package com.example.instagram
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InstagramJoin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insta_join)
+
+    }
+}
