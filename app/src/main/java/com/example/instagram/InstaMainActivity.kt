4df98c3f77f26e49d86945dbb9cc264770d6eb51
soyeon <sykim0659@naver.com>
2024-09-02 20:08:41 +0900

    메뉴바 기능 구현

--- app/src/main/java/com/example/instagram/InstaMainActivity.kt
+++ app/src/main/java/com/example/instagram/InstaMainActivity.kt
@@ -5,7 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
 
 class InstaMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,38 @@ class InstaMainActivity : AppCompatActivity() {
         tabs.addTab(tabs.newTab().setIcon(R.drawable.btn_outsta_home))
         tabs.addTab(tabs.newTab().setIcon(R.drawable.btn_outsta_post))
         tabs.addTab(tabs.newTab().setIcon(R.drawable.btn_outsta_my))
+
+        val pager = findViewById<ViewPager2>(R.id.main_pager)
+        pager.adapter = InstaMainPagerAdapter(this, 3)
+
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: Tab?) {
+                pager.setCurrentItem(tab!!.position)
+            }
+
+            override fun onTabUnselected(tab: Tab?) {
+            }
+
+            override fun onTabReselected(tab: Tab?) {
+            }
+        })
+    }
+}
+
+class InstaMainPagerAdapter(
+    fragmentActivity : FragmentActivity,
+    val tabCount : Int
+    ) : FragmentStateAdapter(fragmentActivity){
+    override fun getItemCount(): Int {
+        return tabCount
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            0-> return InstaFeedFragment()
+            1 -> return InstaPostFragment()
+            else -> return InstaProfileFragment()
+
+        }
     }
 }
