dd5210e98394cf231b08b9b6a3b275c6dcb4981f
soyeon <sykim0659@naver.com>
2024-09-02 22:14:45 +0900

    피드 기능 구현

--- app/src/main/java/com/example/instagram/InstaFeedFragment.kt
+++ app/src/main/java/com/example/instagram/InstaFeedFragment.kt
@@ -1,10 +1,17 @@
 package com.example.instagram
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class InstaFeedFragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +20,33 @@ class InstaFeedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.insta_feed_fragment,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val feedListView = view.findViewById<RecyclerView>(R.id.feed_list)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val retrofitService = retrofit.create(RetrofitService::class.java)
+
+        retrofitService.getInstagramPosts().enqueue(object  : Callback<ArrayList<InstaPost>>{
+            override fun onResponse(
+                call: Call<ArrayList<InstaPost>>,
+                response: Response<ArrayList<InstaPost>>
+            ) {
+                val postlist = response.body()
+                postlist!!.forEach{
+                    Log.d("instaa", it.content)
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<InstaPost>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
 }
--- app/src/main/java/com/example/instagram/RetrofitService.kt
+++ app/src/main/java/com/example/instagram/RetrofitService.kt
@@ -3,13 +3,27 @@ package com.example.instagram
 import retrofit2.Call
 import retrofit2.http.FieldMap
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 class User(
     val token: String, val username: String, val id: Int
 )
 
+class InstaPost(
+    val content: String, val image: String, val owner_profile: OwnerProfile
+)
+
+class OwnerProfile(
+    val username: String, val image: String
+)
+
 interface RetrofitService {
+
+    @GET("instagram/post/list/all/")
+    fun getInstagramPosts(
+    ):Call<ArrayList<InstaPost>>
+
     @POST("user/login/")
     @FormUrlEncoded
     fun instaLogin(
