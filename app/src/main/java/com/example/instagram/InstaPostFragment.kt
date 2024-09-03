64756acd67b40c79434165d0c48b20abf628e31d
soyeon <sykim0659@naver.com>
2024-09-03 16:37:02 +0900

    포스트 업로드 기능 구현

--- app/src/main/java/com/example/instagram/InstaPostFragment.kt
+++ app/src/main/java/com/example/instagram/InstaPostFragment.kt
@@ -1,12 +1,22 @@
 package com.example.instagram
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.selects.select
 
 class InstaPostFragment: Fragment() {
+    var imageUri: Uri? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,4 +25,21 @@ class InstaPostFragment: Fragment() {
         return inflater.inflate(R.layout.insta_post_fragment,container,false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val selectedImageView = view.findViewById<ImageView>(R.id.selected_img)
+        val glide = Glide.with(activity as InstaMainActivity)
+
+        val ImagePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                imageUri = it.data!!.data
+                glide.load(imageUri).into(selectedImageView)
+            }
+        ImagePickerLauncher.launch(
+            Intent(Intent.ACTION_PICK).apply {
+                this.type = MediaStore.Images.Media.CONTENT_TYPE
+            }
+        )
+    }
 }
