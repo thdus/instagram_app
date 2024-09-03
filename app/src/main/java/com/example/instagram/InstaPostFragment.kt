1e1e577131aa57262fb0bf3807052b93a3a86ca8
soyeon <sykim0659@naver.com>
2024-09-03 17:19:32 +0900

    포스트 업로드 기능 구현

--- app/src/main/java/com/example/instagram/InstaPostFragment.kt
+++ app/src/main/java/com/example/instagram/InstaPostFragment.kt
@@ -1,6 +1,8 @@
 package com.example.instagram
 
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -8,14 +10,27 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.selects.select
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 class InstaPostFragment: Fragment() {
     var imageUri: Uri? = null
+    var contentInput: String =""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,5 +56,67 @@ class InstaPostFragment: Fragment() {
                 this.type = MediaStore.Images.Media.CONTENT_TYPE
             }
         )
+        view.findViewById<EditText>(R.id.selected_content).doAfterTextChanged {
+            contentInput = it.toString()
+        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val retrofitService = retrofit.create(RetrofitService::class.java)
+        view.findViewById<TextView>(R.id.upload).setOnClickListener {
+            val file = getRealFile(imageUri!!)
+            val requestFile = RequestBody.create(
+                MediaType.parse(
+                (activity as InstaMainActivity).contentResolver.getType(imageUri!!)
+            ), file
+            )
+            val body = MultipartBody.Part.createFormData("image", file!!.name, requestFile)
+            val content = RequestBody.create(MultipartBody.FORM, contentInput)
+            val header = HashMap<String, String>()
+            val sp = (activity as InstaMainActivity).getSharedPreferences(
+                "user_info",
+                Context.MODE_PRIVATE)
+            val token = sp.getString("token", "")
+            header.put("Authorization", token!!)
+
+            retrofitService.uploadPost(header, body, content).enqueue(object : Callback<Any>{
+                override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFailure(call: Call<Any>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+        }
+    }
+
+    private fun getRealFile(uri: Uri): File? {
+        var uri: Uri? = uri
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        if (uri == null) {
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+        var cursor: Cursor? = (activity as InstaMainActivity).getContentResolver().query(
+            uri!!,
+            projection,
+            null,
+            null,
+            MediaStore.Images.Media.DATE_MODIFIED + "desc"
+        )
+        if (cursor == null || cursor.getColumnCount() < 1) {
+            return null
+        }
+        val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor.moveToFirst()
+        val path: String = cursor.getString(column_index)
+        if (cursor != null) {
+            cursor.close()
+            cursor = null
+        }
+        return File(path)
     }
 }
--- app/src/main/java/com/example/instagram/RetrofitService.kt
+++ app/src/main/java/com/example/instagram/RetrofitService.kt
@@ -1,10 +1,15 @@
 package com.example.instagram
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.FieldMap
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.HeaderMap
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 import java.io.File
 
@@ -23,6 +28,14 @@ class OwnerProfile(
 
 
 interface RetrofitService {
+
+    @Multipart
+    @POST("instagram/post/")
+    fun uploadPost(
+        @HeaderMap headers: Map<String, String>,
+        @Part image : MultipartBody.Part,
+        @Part("content") content : RequestBody
+    ): Call<Any>
 
     @POST("instagram/post/like/{post_id}/")
     fun postLike(
